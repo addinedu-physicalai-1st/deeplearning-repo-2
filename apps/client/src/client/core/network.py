c337071cf5a0d9bbfcff1d500b7e7a02ac90aed5
jinw0o0 <rlawlsdn4665@gmail.com>
2026-02-04 17:17:56 +0900

    실행시간이 적어도 report 나오게 수정

--- apps/client/src/client/core/network.py
+++ apps/client/src/client/core/network.py
@@ -47,7 +47,7 @@ class NetworkClient:
         url = self.operation_url.replace("/inference", f"/sessions/stop/{session_id}")
         headers = {"X-API-Key": self.api_key}
         try:
-            response = requests.post(url, headers=headers, timeout=5, verify=True)
+            response = requests.post(url, headers=headers, timeout=40, verify=True)
             response.raise_for_status()
             return response.json()
         except Exception as e:
